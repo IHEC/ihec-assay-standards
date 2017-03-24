23c347131eda6262e4b4d173f2b91ca623a81939
sitag <sitag@users.noreply.github.com>
2017-03-23 18:10:00 -0700

    wait + & on samtools, remove before production

--- RNASeqQC/rna_stats2.sh
+++ RNASeqQC/rna_stats2.sh
@@ -32,11 +32,17 @@ cd $WORKING_DIR/$SAMPLE_NAME
 
 #computation of IHEC RNA-seq stats
 #get the fraction of mapped reads
-MAPPED_READS=$($SAMTOOLS_131/samtools view -F 0x904 -c $SAMPLE_PATH) 
+$SAMTOOLS_131/samtools view -F 0x904 -c $SAMPLE_PATH > $SAMPLE_NAME.n_mapped &
+PID=$! # send this process into background and wait on it before reading $SAMPLE_NAME.n_mapped
 
 # create 0x900 filtered bam and count those reads at the same time with tee redireting to no_multimap_bam
 # faster because sone concurrently + no uncompressed data streamed
-$SAMTOOLS_131/samtools view -b -F 0x900 $SAMPLE_PATH | tee ${SAMPLE_NAME}_no_multimap.bam | $SAMTOOLS_131/samtools view -c - | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_MAPPED\t"MAPPED_READS/$1}' > ${SAMPLE_NAME}_read_stats.txt
+N_NO_MULTIMAP=$($SAMTOOLS_131/samtools view -b -F 0x900 $SAMPLE_PATH | tee ${SAMPLE_NAME}_no_multimap.bam | $SAMTOOLS_131/samtools view -c -)
+
+# wait on $SAMPLE_NAME.n_mapped process
+wait $PID 
+MAPPED_READS=$(cat $SAMPLE_NAME.n_mapped) 
+echo $N_NO_MULTIMAP | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_MAPPED\t"MAPPED_READS/$1}' > ${SAMPLE_NAME}_read_stats.txt
 
 #get the number of reads falling into intergenic regions with samtools
 $SAMTOOLS_131/samtools view -c ${SAMPLE_NAME}_no_multimap.bam -L $INTERGENIC_BED | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_INTERGENIC\t"$1/MAPPED_READS}' >> ${SAMPLE_NAME}_read_stats.txt
@@ -50,9 +56,8 @@ PICARD_MARK_DUP_CMD="java -Xmx4g -jar $PICARD_290/picard.jar MarkDuplicates VALI
 if [[ -z $TMP_DIR ]]; then
   $PICARD_MARK_DUP_CMD
 else
-  TMP_DIR=$TMP_DIR
   TEMP="$TMP_DIR/$SAMPLE_NAME/temp"
-  mkdir -p $TEMP
+  mkdir -p $TEMP || true
   $PICARD_MARK_DUP_CMD TMP_DIR=$TEMP
 fi  
 awk -F $'\t' '/PERCENT_DUPLICATION/{getline; print "FRACTION_DUPLICATED\t"$8}' ${SAMPLE_NAME}_duplicated.txt >> ${SAMPLE_NAME}_read_stats.txt
