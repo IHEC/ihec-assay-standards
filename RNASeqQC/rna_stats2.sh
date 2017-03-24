fa04782f1fd53d6e8b2a34088862ed50af975e83
sitag <sitag@users.noreply.github.com>
2017-03-23 17:47:09 -0700

    beef up + add missing bam creation

--- RNASeqQC/rna_stats2.sh
+++ RNASeqQC/rna_stats2.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+set -eufx -o pipefail # -x is noisy but keep on till in production 
+
 if [ $# -lt 3 ]; then
-	echo
-	echo "   USAGE: $0 <SAMPLE_PATH> <INTERGENIC_BED> <RRNA_BED>"
-	echo "   The script computes RNA-seq qualtity metrics according to IHEC standards"
-	echo ""
-	echo "   Input files in the following order are required:"
-	echo "   1. path to BAM file to be analyzed"
-	echo "   2. Bed file with intergenic coordinates"
-	echo "   3. Bed file with rRNA coordinates"
-	echo
-	exit 1
+  echo
+  echo "   USAGE: $0 <SAMPLE_PATH> <INTERGENIC_BED> <RRNA_BED>"
+  echo "   The script computes RNA-seq qualtity metrics according to IHEC standards"
+  echo ""
+  echo "   Input files in the following order are required:"
+  echo "   1. path to BAM file to be analyzed"
+  echo "   2. Bed file with intergenic coordinates"
+  echo "   3. Bed file with rRNA coordinates"
+  echo
+  exit 1
+fi
+
+if [[ -z $PICARD_290 || -z $SAMTOOLS_131 ]]; then 
+  echo "Environment variable PICARD_290 (path to picard jar v2.9.1) and SAMTOOLS_131 (samtools 1.3.1 path) must be set"
+  exit 1
 fi
 
 SAMPLE_PATH=$1
@@ -19,26 +26,37 @@ RRNA_BED=$3
 
 SAMPLE_NAME=$(basename $SAMPLE_PATH .bam)
 
-mkdir $SAMPLE_NAME
-
-cd $SAMPLE_NAME
+WORKING_DIR=${QC_DIR:-"."} # by default working directory is current
+mkdir -p $WORKING_DIR/$SAMPLE_NAME || true
+cd $WORKING_DIR/$SAMPLE_NAME
 
 #computation of IHEC RNA-seq stats
 #get the fraction of mapped reads
-MAPPED_READS=$(samtools view -F 0x904 -c $SAMPLE_PATH)
+MAPPED_READS=$($SAMTOOLS_131/samtools view -F 0x904 -c $SAMPLE_PATH) 
 
-samtools view -F 0x900 -c $SAMPLE_PATH | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_MAPPED\t"MAPPED_READS/$1}' > ${SAMPLE_NAME}_read_stats.txt
+# create 0x900 filtered bam and count those reads at the same time with tee redireting to no_multimap_bam
+# faster because sone concurrently + no uncompressed data streamed
+$SAMTOOLS_131/samtools view -b -F 0x900 $SAMPLE_PATH | tee ${SAMPLE_NAME}_no_multimap.bam | $SAMTOOLS_131/samtools view -c - | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_MAPPED\t"MAPPED_READS/$1}' > ${SAMPLE_NAME}_read_stats.txt
 
 #get the number of reads falling into intergenic regions with samtools
-samtools view -c -F 0x900 $SAMPLE_PATH -L $INTERGENIC_BED | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_INTERGENIC\t"$1/MAPPED_READS}' >> ${SAMPLE_NAME}_read_stats.txt
+$SAMTOOLS_131/samtools view -c ${SAMPLE_NAME}_no_multimap.bam -L $INTERGENIC_BED | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_INTERGENIC\t"$1/MAPPED_READS}' >> ${SAMPLE_NAME}_read_stats.txt
 
 #get the number of reads from rRNA with samtools
-samtools view -c -F 0x900 $SAMPLE_PATH -L $RRNA_BED | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_RRNA\t"$1/MAPPED_READS}' >> ${SAMPLE_NAME}_read_stats.txt
+$SAMTOOLS_131/samtools view -c ${SAMPLE_NAME}_no_multimap.bam -L $RRNA_BED | awk -v MAPPED_READS=$MAPPED_READS '{print "FRACTION_RRNA\t"$1/MAPPED_READS}' >> ${SAMPLE_NAME}_read_stats.txt
 
 #get number of duplicates
-java -Xmx4g -jar $PICARD/picard.jar MarkDuplicates VALIDATION_STRINGENCY=LENIENT I=${SAMPLE_NAME}_no_multimap.bam O=${SAMPLE_NAME}_noMULTI_noDUP.bam M=${SAMPLE_NAME}_duplicated.txt
-
+PICARD_MARK_DUP_CMD="java -Xmx4g -jar $PICARD_290/picard.jar MarkDuplicates VALIDATION_STRINGENCY=LENIENT I=${SAMPLE_NAME}_no_multimap.bam O=${SAMPLE_NAME}_noMULTI_noDUP.bam M=${SAMPLE_NAME}_duplicated.txt"
+#add -TMP_DIR argument to mark dups so avoid running out of space. 
+if [[ -z $TMP_DIR ]]; then
+  $PICARD_MARK_DUP_CMD
+else
+  TMP_DIR=$TMP_DIR
+  TEMP="$TMP_DIR/$SAMPLE_NAME/temp"
+  mkdir -p $TEMP
+  $PICARD_MARK_DUP_CMD TMP_DIR=$TEMP
+fi  
 awk -F $'\t' '/PERCENT_DUPLICATION/{getline; print "FRACTION_DUPLICATED\t"$8}' ${SAMPLE_NAME}_duplicated.txt >> ${SAMPLE_NAME}_read_stats.txt
 
-rm ${SAMPLE_NAME}_noMULTI_noDUP.bam
-rm ${SAMPLE_NAME}_duplicated.txt
+# keep everything while debugging  
+#rm ${SAMPLE_NAME}_noMULTI_noDUP.bam 
+#rm ${SAMPLE_NAME}_duplicated.txt
