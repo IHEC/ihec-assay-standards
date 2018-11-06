028e6cd2b4f57dddd276b9c01c3f8fa9112b2684
sitag <sgakkhar@bcgsc.ca>
2018-11-05 22:22:39 -0800

    more clean up

---
+++ pipelines/chip-seq-encode-wrapper/testrun_tasks_template.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+unset PYTHONPATH
+unset R_LIBS_USER
+which R
+which python
+which java
+
+echo "paths: $R_LIBS_USER $PYTHONPATH"
+echo $PATH
+
+
+CROMWELL_HOME="{home}"
+BACKEND_CONF="{backend}"
+WORKFLOW_OPT="{container}"
+BACKEND="Local"
+
+
+WDL="$1"
+
+PREFIX=$(basename $WDL .wdl)
+METADATA="$PREFIX".metadata.json # metadata
+RESULT=$3
+
+
+jobFile=$2 
+
+java -jar -Dconfig.file=$BACKEND_CONF -Dbackend.default=$BACKEND $CROMWELL_HOME/cromwell-34.jar run $WDL -i $jobFile -o $WORKFLOW_OPT -m $METADATA
+echo "return:$?"
+
+cat $METADATA | python -c "import json,sys;obj=json.load(sys.stdin);print(obj['outputs']['"$PREFIX".compare_md5sum.json_str'])" > $RESULT
+cat $RESULT
+
+
+
+
+
